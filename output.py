1bf599d248e1f704296ee0a98e95dc0675d307b5
yshi128888 <crystal0102@foxmail.com>
2019-05-03 16:48:17 -0700

    solve some indent issue

--- output.py
+++ output.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
 	PATH = 'G_epoch0.pth.tar'
 
 	save_path = 'output'
-    ori_path = 'demoOriginal'
+	ori_path = 'demoOriginal'
 
 	model_G = ConvGen(ngf)
 	checkpoint_G = torch.load(PATH)
@@ -16,22 +16,22 @@ if __name__ == '__main__':
 	model_G.eval()
 
 	val_loader = get_loader(os.path.join(ori_path, 'data/'),
-                            batch_size=args.batch_size,
-                            large=args.large,
-                            mode='val',
-                            num_workers=4,
-                            )
+		batch_size=args.batch_size,
+		large=args.large,
+		mode='val',
+		num_workers=4,
+		)
 	val_bs = val_loader.batch_size
 	with torch.no_grad(): # Fuck torch.no_grad!! Gradient will accumalte if you don't set torch.no_grad()!!
-        for i, (data, target) in enumerate(val_loader):
-            data, target = Variable(data.cuda()), Variable(target.cuda())
-            fake =  model_G(data).data
-            for i in range(val_bs):
-	            # validate with fake
-		        pred = fake[i].cpu().numpy()
-		        pred_rgb = (np.transpose(pred, (1,2,0)).astype(np.float64) + 1) / 2.
-		        plt.figure(figsize=(480,360))
-			    plt.imshow(pred_rgb)
-			    plt.axis('off')
-			    plt.tight_layout()
-			    plt.savefig(os.path.join(data_root, '%05d.png'))
+		for i, (data, target) in enumerate(val_loader):
+			data, target = Variable(data.cuda()), Variable(target.cuda())
+			fake =  model_G(data).data
+			for i in range(val_bs):
+				# validate with fake
+				pred = fake[i].cpu().numpy()
+				pred_rgb = (np.transpose(pred, (1,2,0)).astype(np.float64) + 1) / 2.
+				plt.figure(figsize=(480,360))
+				plt.imshow(pred_rgb)
+				plt.axis('off')
+				plt.tight_layout()
+				plt.savefig(os.path.join(data_root, '%05d.png'))
