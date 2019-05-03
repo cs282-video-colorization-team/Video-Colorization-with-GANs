a1689b51220798ff179a7983d24a03cd664aab1c
Lichen Wang <arjen@berkeley.edu>
2019-05-02 17:01:41 -0700

    smaller structure for both G and D

--- gan_model.py
+++ gan_model.py
@@ -9,43 +9,43 @@ class ConvGen(nn.Module):
     def __init__(self):
         super(ConvGen, self).__init__()
 
-        self.conv1 = nn.Conv2d(1, 64, 3, stride=2, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
+        self.conv1 = nn.Conv2d(1, 32, 3, stride=2, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(32)
         self.relu1 = nn.LeakyReLU(0.1)
 
-        self.conv2 = nn.Conv2d(64, 128, 3, stride=2, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(128)
+        self.conv2 = nn.Conv2d(32, 64, 3, stride=2, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(64)
         self.relu2 = nn.LeakyReLU(0.1)
 
-        self.conv3 = nn.Conv2d(128, 256, 3, stride=2, padding=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(256)
+        self.conv3 = nn.Conv2d(64, 128, 3, stride=2, padding=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(128)
         self.relu3 = nn.LeakyReLU(0.1)
 
-        self.conv4 = nn.Conv2d(256, 512, 3, stride=2, padding=1, bias=False)
-        self.bn4 = nn.BatchNorm2d(512)
+        self.conv4 = nn.Conv2d(128, 256, 3, stride=2, padding=1, bias=False)
+        self.bn4 = nn.BatchNorm2d(256)
         self.relu4 = nn.LeakyReLU(0.1)
 
-        self.conv5 = nn.Conv2d(512, 512, 3, stride=2, padding=1, bias=False)
-        self.bn5 = nn.BatchNorm2d(512)
+        self.conv5 = nn.Conv2d(256, 256, 3, stride=2, padding=1, bias=False)
+        self.bn5 = nn.BatchNorm2d(256)
         self.relu5 = nn.LeakyReLU(0.1)
 
-        self.deconv6 = nn.ConvTranspose2d(512, 512, 3, stride=2, padding=1, output_padding=1, bias=False)
-        self.bn6 = nn.BatchNorm2d(512)
+        self.deconv6 = nn.ConvTranspose2d(256, 256, 3, stride=2, padding=1, output_padding=1, bias=False)
+        self.bn6 = nn.BatchNorm2d(256)
         self.relu6 = nn.ReLU()
 
-        self.deconv7 = nn.ConvTranspose2d(512, 256, 3, stride=2, padding=1, output_padding=1, bias=False)
-        self.bn7 = nn.BatchNorm2d(256)
+        self.deconv7 = nn.ConvTranspose2d(256, 128, 3, stride=2, padding=1, output_padding=1, bias=False)
+        self.bn7 = nn.BatchNorm2d(128)
         self.relu7 = nn.ReLU()
 
-        self.deconv8 = nn.ConvTranspose2d(256, 128, 3, stride=2, padding=1, output_padding=1, bias=False)
-        self.bn8 = nn.BatchNorm2d(128)
+        self.deconv8 = nn.ConvTranspose2d(128, 64, 3, stride=2, padding=1, output_padding=1, bias=False)
+        self.bn8 = nn.BatchNorm2d(64)
         self.relu8 = nn.ReLU()
 
-        self.deconv9 = nn.ConvTranspose2d(128, 64, 3, stride=2, padding=1, output_padding=1, bias=False)
-        self.bn9 = nn.BatchNorm2d(64)
+        self.deconv9 = nn.ConvTranspose2d(64, 32, 3, stride=2, padding=1, output_padding=1, bias=False)
+        self.bn9 = nn.BatchNorm2d(32)
         self.relu9 = nn.ReLU()
 
-        self.deconv10 = nn.ConvTranspose2d(64, 3, 3, stride=2, padding=1, output_padding=1, bias=False)
+        self.deconv10 = nn.ConvTranspose2d(32, 3, 3, stride=2, padding=1, output_padding=1, bias=False)
         self.bn10 = nn.BatchNorm2d(3)
         self.relu10 = nn.ReLU()
 
@@ -323,37 +323,37 @@ class ConvGenTime(nn.Module):
 
 class ConvDis(nn.Module):
     '''Discriminator'''
-    def __init__(self, large=False):
+    def __init__(self, large=True):
         super(ConvDis, self).__init__()
 
-        self.conv1 = nn.Conv2d(3, 64, 3, stride=2, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
+        self.conv1 = nn.Conv2d(3, 32, 3, stride=2, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(32)
         self.relu1 = nn.LeakyReLU(0.1)
 
-        self.conv2 = nn.Conv2d(64, 128, 3, stride=2, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(128)
+        self.conv2 = nn.Conv2d(32, 64, 3, stride=2, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(64)
         self.relu2 = nn.LeakyReLU(0.1)
 
-        self.conv3 = nn.Conv2d(128, 256, 3, stride=2, padding=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(256)
+        self.conv3 = nn.Conv2d(64, 128, 3, stride=2, padding=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(128)
         self.relu3 = nn.LeakyReLU(0.1)
 
-        self.conv4 = nn.Conv2d(256, 512, 3, stride=2, padding=1, bias=False)
-        self.bn4 = nn.BatchNorm2d(512)
+        self.conv4 = nn.Conv2d(128, 256, 3, stride=2, padding=1, bias=False)
+        self.bn4 = nn.BatchNorm2d(256)
         self.relu4 = nn.LeakyReLU(0.1)
 
-        self.conv5 = nn.Conv2d(512, 512, 3, stride=2, padding=1, bias=False)
-        self.bn5 = nn.BatchNorm2d(512)
+        self.conv5 = nn.Conv2d(256, 256, 3, stride=2, padding=1, bias=False)
+        self.bn5 = nn.BatchNorm2d(256)
         self.relu5 = nn.LeakyReLU(0.1)
 
         if large:
-            self.conv6 = nn.Conv2d(512, 512, 15, stride=1, padding=0, bias=False)
+            self.conv6 = nn.Conv2d(256, 256, 15, stride=1, padding=0, bias=False)
         else:
-            self.conv6 = nn.Conv2d(512, 512, 7, stride=1, padding=0, bias=False)
-        self.bn6 = nn.BatchNorm2d(512)
+            self.conv6 = nn.Conv2d(256, 256, 7, stride=1, padding=0, bias=False)
+        self.bn6 = nn.BatchNorm2d(256)
         self.relu6 = nn.LeakyReLU(0.1)
 
-        self.conv7 = nn.Conv2d(512, 1, 1, stride=1, padding=0, bias=False)
+        self.conv7 = nn.Conv2d(256, 1, 1, stride=1, padding=0, bias=False)
 
         self._initialize_weights()
 
