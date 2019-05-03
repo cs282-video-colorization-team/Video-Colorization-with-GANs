9aa4cf181699f57dc61a981dc5a63a610f095776
Lichen Wang <arjen@berkeley.edu>
2019-05-02 21:51:43 -0700

    seperate main and data loader file

---
+++ gan_model_time.py
@@ -0,0 +1,212 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ConvGenTime(nn.Module):
+    '''Generator'''
+    def __init__(self, ngf=32):
+        super(ConvGenTime, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, ngf, 3, stride=2, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(ngf)
+        self.relu1 = nn.LeakyReLU(0.1)
+
+        self.conv2 = nn.Conv2d(ngf, ngf*2, 3, stride=2, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(ngf*2)
+        self.relu2 = nn.LeakyReLU(0.1)
+
+        self.conv3 = nn.Conv2d(ngf*2, ngf*4, 3, stride=2, padding=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(ngf*4)
+        self.relu3 = nn.LeakyReLU(0.1)
+
+        self.conv4 = nn.Conv2d(ngf*4, ngf*8, 3, stride=2, padding=1, bias=False)
+        self.bn4 = nn.BatchNorm2d(ngf*8)
+        self.relu4 = nn.LeakyReLU(0.1)
+
+        self.conv5 = nn.Conv2d(ngf*8, ngf*8, 3, stride=2, padding=1, bias=False)
+        self.bn5 = nn.BatchNorm2d(ngf*8)
+        self.relu5 = nn.LeakyReLU(0.1)
+
+        # === 1x1 cov for prev, now, next
+        self.conv1x1prev = nn.Conv2d(ngf*8, ngf, 1, stride=1, padding=0, bias=False)
+        self.conv1x1now = nn.Conv2d(ngf*8, ngf*6, 1, stride=1, padding=0, bias=False)
+        self.conv1x1next = nn.Conv2d(ngf*8, ngf, 1, stride=1, padding=0, bias=False)
+        self.bn1x1 = nn.BatchNorm2d(ngf*8)
+        self.relu1x1 = nn.LeakyReLU(0.1)
+        # ===
+
+        self.deconv6 = nn.ConvTranspose2d(ngf*8, ngf*8, 3, stride=2, padding=1, output_padding=1, bias=False)
+        self.bn6 = nn.BatchNorm2d(ngf*8)
+        self.relu6 = nn.ReLU()
+
+        self.deconv7 = nn.ConvTranspose2d(ngf*8, ngf*4, 3, stride=2, padding=1, output_padding=1, bias=False)
+        self.bn7 = nn.BatchNorm2d(ngf*4)
+        self.relu7 = nn.ReLU()
+
+        self.deconv8 = nn.ConvTranspose2d(ngf*4, ngf*2, 3, stride=2, padding=1, output_padding=1, bias=False)
+        self.bn8 = nn.BatchNorm2d(ngf*2)
+        self.relu8 = nn.ReLU()
+
+        self.deconv9 = nn.ConvTranspose2d(ngf*2, ngf, 3, stride=2, padding=1, output_padding=1, bias=False)
+        self.bn9 = nn.BatchNorm2d(ngf)
+        self.relu9 = nn.ReLU()
+
+        self.deconv10 = nn.ConvTranspose2d(ngf, 3, 3, stride=2, padding=1, output_padding=1, bias=False)
+        self.bn10 = nn.BatchNorm2d(3)
+        self.relu10 = nn.ReLU()
+
+        self._initialize_weights()
+
+    def forward(self, _now, _prev, _next):
+
+
+        # ==========================
+        # Step 1  -START- now branch 
+        # ==========================
+
+        h_now = _now
+        h_now = self.conv1(h_now)
+        h_now = self.bn1(h_now)
+        h_now = self.relu1(h_now) # 64,112,112 (if input is 224x224)
+        pool1 = h_now
+
+        h_now = self.conv2(h_now)
+        h_now = self.bn2(h_now)
+        h_now = self.relu2(h_now) # 128,56,56
+        pool2 = h_now
+
+        h_now = self.conv3(h_now) # 256,28,28
+        h_now = self.bn3(h_now)
+        h_now = self.relu3(h_now)
+        pool3 = h_now
+
+        h_now = self.conv4(h_now) # 512,14,14
+        h_now = self.bn4(h_now)
+        h_now = self.relu4(h_now)
+        pool4 = h_now
+
+        h_now = self.conv5(h_now) # 512,7,7
+        h_now = self.bn5(h_now)
+        h_now = self.relu5(h_now) # >>>>> Bottleneck
+
+        h_now =self.conv1x1now(h_now) # 1x1conv
+
+        # ==========================
+        # Step 1    -END- now branch 
+        # ==========================
+
+        # ========================
+        # Step 2  -START- prev branch 
+        # =========================
+
+        h_prev = _prev
+        h_prev = self.conv1(h_prev)
+        h_prev = self.bn1(h_prev)
+        h_prev = self.relu1(h_prev) # 64,112,112 (if input is 224x224)
+
+        h_prev = self.conv2(h_prev)
+        h_prev = self.bn2(h_prev)
+        h_prev = self.relu2(h_prev) # 128,56,56
+
+        h_prev = self.conv3(h_prev) # 256,28,28
+        h_prev = self.bn3(h_prev)
+        h_prev = self.relu3(h_prev)
+
+        h_prev = self.conv4(h_prev) # 512,14,14
+        h_prev = self.bn4(h_prev)
+        h_prev = self.relu4(h_prev)
+
+        h_prev = self.conv5(h_prev) # 512,7,7
+        h_prev = self.bn5(h_prev)
+        h_prev = self.relu5(h_prev) # >>>>> Bottleneck
+
+        h_prev =self.conv1x1prev(h_prev) # 1x1conv
+
+        # ========================
+        # Step 2    -END- prev branch 
+        # ========================
+
+        # ========================
+        # Step 3  -START- next branch 
+        # =========================
+
+        h_next = _next
+        h_next = self.conv1(h_next)
+        h_next = self.bn1(h_next)
+        h_next = self.relu1(h_next) # 64,112,112 (if input is 224x224)
+
+        h_next = self.conv2(h_next)
+        h_next = self.bn2(h_next)
+        h_next = self.relu2(h_next) # 128,56,56
+
+        h_next = self.conv3(h_next) # 256,28,28
+        h_next = self.bn3(h_next)
+        h_next = self.relu3(h_next)
+
+        h_next = self.conv4(h_next) # 512,14,14
+        h_next = self.bn4(h_next)
+        h_next = self.relu4(h_next)
+
+        h_next = self.conv5(h_next) # 512,7,7
+        h_next = self.bn5(h_next)
+        h_next = self.relu5(h_next) # >>>>> Bottleneck
+
+        h_next =self.conv1x1next(h_next) # 1x1conv
+
+        # ========================
+        # Step 3    -END- next branch 
+        # ========================
+
+
+        # =========================
+        # Step 4  -START- concat feature map
+        # =========================
+        h = torch.cat((h_prev, h_now, h_next), dim=1)
+        h = self.bn1x1(h)
+        h = self.relu1x1(h) # 512,7,7
+
+        # =========================
+        # Step 4  -END- concat feature map
+        # =========================
+
+        # =========================
+        # Step 4  -START- UNET decoder
+        # =========================
+        h = self.deconv6(h)
+        h = self.bn6(h)
+        h = self.relu6(h) # 512,14,14
+        h += pool4
+
+        h = self.deconv7(h)
+        h = self.bn7(h)
+        h = self.relu7(h) # 256,28,28
+        h += pool3
+
+        h = self.deconv8(h)
+        h = self.bn8(h)
+        h = self.relu8(h) # 128,56,56
+        h += pool2
+
+        h = self.deconv9(h)
+        h = self.bn9(h)
+        h = self.relu9(h) # 64,112,112
+        h += pool1
+
+        h = self.deconv10(h)
+        h = F.tanh(h) # 3,224,224
+
+        # =========================
+        # Step 4  -END- UNET decoder
+        # =========================
+
+        return h
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            if isinstance(m, nn.ConvTranspose2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
--- movie_data_loader.py
+++ movie_data_loader.py
@@ -41,54 +41,6 @@ class Movie(Dataset):
     def __len__(self):
         return len(self.data_files_name)
 
-class MovieTime(Dataset):
-    def __init__(self, image_path, transform_color, transform_gray, mode):
-        self.image_path = image_path
-        self.transform_color = transform_color
-        self.transform_gray = transform_gray
-        self.IMAGE_RESIZE = (480, 480)
-        self.mode = mode
-
-        self.data_files_name = [file for file in os.listdir(self.file_path) if os.path.splitext(file)[1] == '.png']
-
-    def __getitem__(self, index):
-
-        now_file_name = self.data_files_name[index]
-        now_timestamp = int(now_file_name.split('.')[0])
-
-        if now_timestamp > 0:
-            prev_timestamp_str = str(now_timestamp - 1)
-            prev_file_name = '0' * (5-len(prev_timestamp_str)) + prev_timestamp_str + '.png'
-        else:
-            prev_file_name = now_file_name
-
-        if now_timestamp < len(self.data_files_name) - 1:
-            next_timestamp_str = str(now_timestamp + 1)
-            next_file_name = '0' * (5-len(next_timestamp_str)) + next_timestamp_str + '.png'
-        else:
-            next_file_name = now_file_name
-
-        # _now, _prev, _next
-        image_now = Image.open(os.path.join(self.image_path + now_file_name))
-        image_now = image.resize(self.IMAGE_RESIZE)
-
-        image_prev = Image.open(os.path.join(self.image_path + prev_file_name))
-        image_prev = image.resize(self.IMAGE_RESIZE)
-
-        image_next = Image.open(os.path.join(self.image_path + next_file_name))
-        image_next = image.resize(self.IMAGE_RESIZE)
-
-        if self.mode == 'train':
-            return self.transform_gray(image_now), self.transform_gray(image_prev), self.transform_gray(image_next), self.transform_color(image)
-        elif self.mode == 'val':
-            return self.transform_gray(image_now), self.transform_gray(image_prev), self.transform_gray(image_next), self.transform_color(image)
-        elif self.mode == 'test':
-            return self.transform_gray(image_now), self.transform_gray(image_prev), self.transform_gray(image_next)
-
-    def __len__(self):
-        return len(self.data_files_name)
-
-
 def get_loader(image_path, batch_size=16, large=True, mode='train', num_workers=1):
     # if large:
     #     crop = transforms.CenterCrop(480)
@@ -117,31 +69,5 @@ def get_loader(image_path, batch_size=16, large=True, mode='train', num_workers=
                                   shuffle=(mode=='train'),
                                   num_workers=num_workers,
                                   drop_last=True) # Fuck drop_last!!!
-
-    return data_loader
-
-
-def get_movie_time_loader(image_path, batch_size=16, mode='train', num_workers=1):
-
-    transform_gray = []
-    transform_gray.append(transforms.Grayscale())
-    transform_gray.append(transforms.ToTensor())
-    transform_gray = transforms.Compose(transform_gray)
-
-    if mode == 'train' or mode == 'val':
-        transform_color = []
-        transform_color.append(transforms.ToTensor())
-        transform_color.append(transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)))
-        transform_color = transforms.Compose(transform_color)
-    else:
-        transform_color = None
-
-    dataset = MovieTime(image_path, transform_color, transform_gray, mode)
-
-    data_loader = data.DataLoader(dataset=dataset,
-                                  batch_size=batch_size,
-                                  shuffle=(mode=='train'),
-                                  num_workers=num_workers,
-                                  drop_last=True)
 
     return data_loader
