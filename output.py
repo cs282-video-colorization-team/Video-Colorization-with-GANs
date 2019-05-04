e8dc5cc31385c56a2a8aa9e3bb9910baaa248c09
yshi128888 <crystal0102@foxmail.com>
2019-05-03 17:59:29 -0700

    debug

--- output.py
+++ output.py
@@ -5,6 +5,7 @@ from movie_data_loader import *
 from torch.autograd import Variable
 import numpy as np
 import matplotlib.pyplot as plt
+from PIL import Image
  
 if __name__ == '__main__':
 	ngf = 64
@@ -25,6 +26,7 @@ if __name__ == '__main__':
 		num_workers=4,
 		)
 	val_bs = val_loader.batch_size
+	cnt = 1
 	with torch.no_grad(): # Fuck torch.no_grad!! Gradient will accumalte if you don't set torch.no_grad()!!
 		for i, (data, target) in enumerate(val_loader):
 			data, target = Variable(data), Variable(target)
@@ -34,12 +36,19 @@ if __name__ == '__main__':
 			for i in range(val_bs):
 				# validate with fake
 				pred = fake[i].cpu().numpy()
-				print("pred shape: ", pred.shape)
+				print (type(pred))
+
 				pred_rgb = (np.transpose(pred, (1,2,0)).astype(np.float64) + 1) / 2.
+
+				# new_im = Image.fromarray(pred_rgb)
+				# new_im.save("numpy_altered_sample2.png" % )
+				# pred_rgb = pred_rgb.resize((480,360))
+				print("pred shape: ", pred_rgb.shape)
 				plt.figure(figsize=(480,360))
 				plt.imshow(pred_rgb)
 				plt.axis('off')
 				plt.tight_layout()
-				plt.savefig(os.path.join(save_path, '%05d.png'))
-				print(os.path.join(save_path, '%05d.png'))
-				pred_rgb.save("output/", '%05d.png')
+				plt.savefig(save_path+ '%05d.png' %(cnt))
+				# print(os.path.join(save_path, '%05d.png'))
+				# pred_rgb.save("output/", '%05d.png' %(cnt))
+
