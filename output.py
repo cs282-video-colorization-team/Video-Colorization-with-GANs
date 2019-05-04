59e0097dbb369ed68bb384c94730c7d5c09df32a
EC2 Default User <ec2-user@ip-172-31-11-240.us-east-2.compute.internal>
2019-05-04 00:27:05 +0000

    debug

--- output.py
+++ output.py
@@ -3,6 +3,8 @@ import os
 from gan_model import *
 from movie_data_loader import *
 from torch.autograd import Variable
+import numpy as np
+import matplotlib.pyplot as plt
  
 if __name__ == '__main__':
 	ngf = 64
@@ -25,7 +27,7 @@ if __name__ == '__main__':
 	val_bs = val_loader.batch_size
 	with torch.no_grad(): # Fuck torch.no_grad!! Gradient will accumalte if you don't set torch.no_grad()!!
 		for i, (data, target) in enumerate(val_loader):
-			data, target = Variable(data.cuda()), Variable(target.cuda())
+			data, target = Variable(data), Variable(target)
 			print(data.shape)
 			fake =  model_G(data).data
 			for i in range(val_bs):
@@ -36,6 +38,6 @@ if __name__ == '__main__':
 				plt.imshow(pred_rgb)
 				plt.axis('off')
 				plt.tight_layout()
-				plt.savefig(os.path.join(data_root, '%05d.png'))
-				print(os.path.join(data_root, '%05d.png'))
-				pred_rgb.save(os.path.join(data_root, '%05d.png'))
+				plt.savefig(os.path.join(save_path, '%05d.png'))
+				print(os.path.join(save_path, '%05d.png'))
+				pred_rgb.save(os.path.join(save_path, '%05d.png'))
